897894b53cdf1a5655c9cbc6c839326ae915d6ed
Elaine Lin <elainewlin@gmail.com>
2019-11-02 21:16:55 -0700

    Updates

---
+++ precache-manifest.7a090983032c932c19a0885833ecd5f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a256f3b50ef8894aea0167b99b77d9",
+    "url": "//elainewlin.github.io/set/index.html"
+  },
+  {
+    "revision": "df5fb6b02c2aa7e55977",
+    "url": "//elainewlin.github.io/set/static/css/main.634fd6dc.chunk.css"
+  },
+  {
+    "revision": "31bfb7090c5b09110e55",
+    "url": "//elainewlin.github.io/set/static/js/2.d4e51034.chunk.js"
+  },
+  {
+    "revision": "df5fb6b02c2aa7e55977",
+    "url": "//elainewlin.github.io/set/static/js/main.5b1c9bf4.chunk.js"
+  },
+  {
+    "revision": "34ebac2368b71ed1a58f",
+    "url": "//elainewlin.github.io/set/static/js/runtime-main.dc8f3e52.js"
+  }
+]);
